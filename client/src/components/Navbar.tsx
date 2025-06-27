b6db9060d73f513ca3100ee1932ad6f7b8bc95a9
ConnerDeFeo <ninjanerozz@gmail.com>
2025-06-26 20:40:39 -0400

    updated it to now have an image

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,20 @@
+import { useNavigate } from "react-router-dom";
 
 const Navbar = ()=>{
+    const navigate = useNavigate();
     return(
         <div className="bg-blue-800 h-12 sm:h-15 md:h-20 flex items-center">
             <div className="flex justify-between w-[95%] mx-auto">
-                <img src="./compass.webp" alt="compassLogo" className="h-8 my-auto bg-white rounded-full border border-blue-800 compassLogo sm:h-12 md:h-15"/>
+                <img 
+                    src="./compass.webp" 
+                    alt="compassLogo" 
+                    className="cursor-pointer h-8 my-auto bg-white rounded-full border border-blue-800 compassLogo sm:h-12 md:h-15"
+                    onClick={() => navigate("/")}
+                />
                 <div className="hidden sm:flex justify-between w-50 w-32 items-center text-white md:text-xl md:w-80">
-                    <p>Services</p>
-                    <p>About</p>
-                    <p>Contact</p>
+                    <p className="cursor-pointer" onClick={() => navigate("/services")}>Services</p>
+                    <p className="cursor-pointer" onClick={() => navigate("/about")}>About</p>
+                    <p className="cursor-pointer" onClick={() => navigate("/contact")}>Contact</p>
                 </div>
             </div>
         </div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,17 @@
 const Home = () => {
     return (
-        <>Testing</>
+        <>
+            <div
+                className="relative w-full h-150 flex items-center justify-center bg-cover bg-center grid text-center text-white"
+                style={{
+                    backgroundImage: `url(./Ruins.jpg)`,
+                }}
+            >
+                <h1 className="text-4xl md:text-6xl font-bold mt-50">Davis Emergency Solutions</h1>
+                <p className="text-2xl md:text-4xl">Navigating Risk, Anchoring Resilience</p>
+            </div>
+            <div></div>
+        </>
     );
 };
 
