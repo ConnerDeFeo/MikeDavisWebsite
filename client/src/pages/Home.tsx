0ddf7641100bcef0975f341365b625cddc18bfcf
ConnerDeFeo <ninjanerozz@gmail.com>
2025-06-27 21:41:27 -0400

    created the simple ionformatino components

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,17 @@
 const Home = () => {
+    const imageStyling = "h-50 w-50 object-cover rounded-xl mx-auto";
+    const titleStyling = "text-2xl text-center font-bold my-3 text-blue-800";
+    const paragraphStyling = "text-center text-lg";
+
+    const InformationComponent = (imageSrc: string, title: string, paragraph: string) => {
+        return (
+            <div>
+                <img src={imageSrc} alt={title} className={imageStyling} />
+                <h2 className={titleStyling}>{title}</h2>
+                <p className={paragraphStyling}>{paragraph}</p>
+            </div>
+        );
+    };
     return (
         <>
             <div
@@ -11,16 +24,28 @@ const Home = () => {
                 <p className="text-2xl md:text-4xl">Navigating Risk, Anchoring Resilience</p>
             </div>
             <div>
-                <div className="border-y-6 border-blue-800 h-3 w-[80%] mx-auto mt-15 flex justify-between items-center h-100">
-                    <div className="border">
-                        <img src="/PrivateOrgsHomePage.jpg"/>
-                    </div>
-                    <div className="border">
-                        <img src="/HealthcareHomePage.jpg"/>
-                    </div>
-                    <div className="border">
-                        <img src="/EducationHomePage.jpg"/>
-                    </div>
+                <div className="grid gap-y-10 md:flex h-3 w-[80%] mx-auto mt-15 justify-center md:justify-between items-center h-100">
+                    {InformationComponent(
+                        "/PrivateOrgsHomePage.jpg",
+                        "Private Organizations",
+                        `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus 
+                        consequat lacinia. Aenean sit amet Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit. Nullam tempus consequat lacinia. Aenean sit amet `
+                    )}
+                    {InformationComponent(
+                        "/HealthcareHomePage.jpg",
+                        "Healthcare Organizations",
+                        `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Nullam tempus consequat lacinia. Aenean sit amet c
+                        adipiscing elit. Nullam tempus consequat lacinia. Aenean sit amet `
+                    )}
+                    {InformationComponent(
+                        "/EducationHomePage.jpg",
+                        "Education Organizations",
+                        `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Nullam tempus consequat lacinia. Aenean sit amet c
+                        adipiscing elit. Nullam tempus consequat lacinia. Aenean sit amet `
+                    )}
                 </div>
             </div>
         </>
