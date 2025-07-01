a7ad7a657c07101707b44dd4f4591c7db937de4e
ConnerDeFeo <ninjanerozz@gmail.com>
2025-06-30 21:51:17 -0400

    on to other pages

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = ()=>{
             <button className="mx-auto border text-2xl text-white bg-blue-800 p-3 rounded-xl cursor-pointer">Explore a Customized Strategy</button>
             <div className="mt-5 bg-linear-to-b from-white from-10% via-cyan-500 via-25% to-blue-800 to-65% h-40 text-white text-2xl flex flex-col justify-end">
                 <p className="mx-auto mb-10 cursor-pointer" onClick={()=>window.scrollTo({top:0,behavior:'smooth'})}>Back To Top</p>
-                <p className="ml-25 mb-3">@2025 Davis Emergency Solutions</p>
+                <p className="ml-5 md:ml-25 mb-3">@2025 Davis Emergency Solutions</p>
             </div>
         </div>
     );
