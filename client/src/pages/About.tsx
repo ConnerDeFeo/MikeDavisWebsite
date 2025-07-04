58d1d8fe1c682861b107aad50463600fd986c21b
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-04 13:24:49 -0400

    working on the about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,6 +1,14 @@
 const About = () => {
     return (
-        <div>About</div>
+        <>
+            <div className="grid md:flex md:flex-row-reverse w-[70%] mx-auto justify-center my-10">
+                <img src="Ruins.jpg" alt="mike davis" className="object-fit h-100 w-80 mx-auto rounded-xl"/>
+                <p className="innerShadow text-2xl p-5">Business Impact","/PrivateBussinessIcon.png","2",`Donec nec nunc tempor,
+                vehicula mi eget, varius nisi. Aliquam at nisi eleifend, commodo tellus eget, rutrum ligula.
+                Donec nec nunc tempor, vehicula mi eget, varius nisi. Aliquam at nisi eleifend, commodo tellus 
+                eget, rutrum ligula.</p>
+            </div>
+        </>
     );
 };
 
