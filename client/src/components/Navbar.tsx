ff299f9fcc8d8e1e75a51046fa8b7c6f3ca06c69
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-09 10:39:04 -0400

    fixed navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,15 +3,15 @@ import { useNavigate } from "react-router-dom";
 const Navbar = ()=>{
     const navigate = useNavigate();
     return(
-        <div className="bg-blue-800 h-12 sm:h-15 md:h-20 flex items-center">
+        <div className="bg-blue-800 h-15 md:h-20 flex items-center">
             <div className="flex justify-between w-[95%] mx-auto">
                 <img 
                     src="./compass.webp" 
                     alt="compassLogo" 
-                    className="cursor-pointer h-8 my-auto bg-white rounded-full border border-blue-800 compassLogo sm:h-12 md:h-15"
+                    className="cursor-pointer my-auto bg-white rounded-full border border-blue-800 compassLogo h-12 md:h-15"
                     onClick={() => navigate("/")}
                 />
-                <div className="hidden sm:flex justify-between w-50 w-32 items-center text-white md:text-xl md:w-80">
+                <div className="flex justify-between w-50 w-32 items-center text-white md:text-xl md:w-80">
                     <p className="cursor-pointer" onClick={() => navigate("/Services")}>Services</p>
                     <p className="cursor-pointer" onClick={() => navigate("/About")}>About</p>
                     <p className="cursor-pointer" onClick={() => navigate("/Contact")}>Contact</p>
